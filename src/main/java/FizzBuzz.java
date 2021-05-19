48195cf723043129b43dc5c29ffa58f1e7d00678
Marta SH <msastreharo@gmail.com>
2021-05-19 17:12:41 +0200

    Refactoring of if statement

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,9 @@
 public class FizzBuzz {
 
     public String fizzBuzzer(int number) {
+        if(number % 3 == 0 && number % 5 == 0) return "FizzBuzz!";
         if(number % 3 == 0) return "Fizz";
         if(number % 5 == 0) return "Buzz";
-        if(number % 3 == 0 && number % 5 == 0) return "FizzBuzz!";
         return number + "";
     }
 
